01d7c1d5e593b77ae18661e1940bba891e323bee
Choiyoonho <cyh7834@gmail.com>
2022-10-26 08:42:37 +0900

    BDD 스타일 Mockito API

--- src/test/java/me/yoonho/inflearnthejavatest/study/StudyServiceTest.java
+++ src/test/java/me/yoonho/inflearnthejavatest/study/StudyServiceTest.java
@@ -15,6 +15,8 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.*;
 
 /**
@@ -79,5 +81,17 @@ public class StudyServiceTest {
 
         // memberService에서 작업이 더이상 일어나면 안된디.
         //verifyNoMoreInteractions(memberService);
+
+        // Mockito BDD
+        // given
+        given(memberService.findById(1L)).willReturn(Optional.of(member));
+        given(studyRepository.save(study)).willReturn(study);
+
+        // when
+        studyService.createNewStudy(1L, study);
+
+        // then
+        then(memberService).should(times(1)).notify(study);
+        then(memberService).shouldHaveNoMoreInteractions();
     }
 }
